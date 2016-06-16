997edaa79aea26b9c788ca53cf8630f6ba41313e
goaaron <goebel.aaron@gmail.com>
2016-06-16 10:28:19 -0700

    a stream of the latest tweets in r/science

--- Python/scienceStream.py
+++ Python/scienceStream.py
@@ -2,11 +2,13 @@ import requests
 import json 
 
 def stream_science_posts():
-	r = requests.get('https://www.reddit.com/r/science/new/posts/.json?limit=10', stream = True, timeout = 10)
-	for post in r.iter_lines():
+	r = requests.session()
+	header = {"User-Agent": "anisotropix Science"}
+	s = r.get('https://www.reddit.com/r/science/new/.json?limit=100', stream = True, headers =header, timeout = 5)#tream = True, timeout = 2)
+	for post in s.iter_lines():
 		if post:
-			json = json.loads(post)
-			yield post
+			print (post)
 
 if __name__ == "__main__":
-    stream_science_posts()
+	while(True):
+         stream_science_posts()
