1d0b0a10f34e8e939780b2a1705cec0fbf261c45
goaaron <goebel.aaron@gmail.com>
2016-06-16 11:14:32 -0700

    added kafka producer

--- Python/scienceStream.py
+++ Python/scienceStream.py
@@ -1,14 +1,28 @@
+import sys
 import requests
 import json 
+from kafka.client import SimpleClient
+from kafka.producer import KeyedProducer
 
-def stream_science_posts():
-	r = requests.session()
-	header = {"User-Agent": "anisotropix Science"}
-	s = r.get('https://www.reddit.com/r/science/new/.json?limit=100', stream = True, headers =header, timeout = 5)#tream = True, timeout = 2)
-	for post in s.iter_lines():
-		if post:
-			print (post)
+class Producer(object):
+
+	def __init__(self, addr):
+		self.client = SimpleClient(addr)
+		self.producer = KeyedProducer(self.client)
+
+	def stream_science_posts(self, key):
+		r = requests.session()
+		header = {"User-Agent": "anisotropix Science"}
+		s = r.get('https://www.reddit.com/r/science/new/.json?limit=100', stream = True, headers =header)#tream = True, timeout = 2)
+		for post in s.iter_lines():
+			if post:
+				self.producer.send_messages('Science_posts',key,  post)
+				print (post)
 
 if __name__ == "__main__":
+	args = sys.argv
+	ip_addr = str(args[1])
+	partition_key = str(arg[2])
+	prod = Producer(ip_addr)
 	while(True):
-         stream_science_posts()
+         prod.stream_science_posts(partition_key)
