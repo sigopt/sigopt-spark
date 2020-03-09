2d749ec4059d9422a1753e769f6a880a5e388d21
Patrick Hayes <403100+pfhayes@users.noreply.github.com>
2020-03-09 14:29:05 -0700

    Update Python version to 1.0.1 (#4)
    * Support Spark 2
    
    * Fix line endings
    
    * Move into scala dir
    
    * Python
    
    * Python example
    
    * Thread through full Param values
    
    * Update python examples
    
    * Update package name
    
    * Error in setup.py
    
    * Typo
    
    * gitignore
    
    * Fix requirement
    
    * mv

--- python/setup.py
+++ python/setup.py
@@ -1,15 +1,14 @@
 from setuptools import setup
 
 setup(
-  name='sigoptspark',
-  version='1.0.0',
+  name='sigopt-spark',
+  version='1.0.1',
   description='SigOpt Pyspark Integration',
   author='SigOpt',
   author_email='support@sigopt.com',
   url='https://sigopt.com/',
-  packages=['sigoptspark'],
-  install_requires=install_requires,
-  install_requires = ['sigopt>=6.0.0', 'pyspark>=2.0.0'],
+  packages=['sigopt_spark'],
+  install_requires = ['sigopt>=5.3.1', 'pyspark>=2.0.0'],
   classifiers=[
     "Development Status :: 5 - Production/Stable",
     "Intended Audience :: Developers",
