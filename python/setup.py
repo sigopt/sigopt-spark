1e6d00e4c95508d242e9a72e0c3ef1a5834946f2
Patrick Hayes <pfhayes@gmail.com>
2020-03-09 14:24:43 -0700

    Fix requirement

--- python/setup.py
+++ python/setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
   name='sigopt-spark',
-  version='1.0.0',
+  version='1.0.1',
   description='SigOpt Pyspark Integration',
   author='SigOpt',
   author_email='support@sigopt.com',
   url='https://sigopt.com/',
   packages=['sigopt_spark'],
-  install_requires = ['sigopt>=6.0.0', 'pyspark>=2.0.0'],
+  install_requires = ['sigopt>=5.3.1', 'pyspark>=2.0.0'],
   classifiers=[
     "Development Status :: 5 - Production/Stable",
     "Intended Audience :: Developers",
